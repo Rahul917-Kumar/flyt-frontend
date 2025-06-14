50a6f284056642cb04227ce80bdbb62fef7fc7e1
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-06-14 13:42:48 +0530

    remove api key

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -98,7 +98,7 @@ export default function PolygonMap({ onDrawComplete, latitude, longitude }: Prop
         if (!window.google) {
             const script = document.createElement("script");
             script.src =
-                "https://maps.googleapis.com/maps/api/js?key=AIzaSyCKLdp5Iqth0HbquleWHenUdYKU6EDVzZc&libraries=drawing";
+                "https://maps.googleapis.com/maps/api/js?key=<api_key>&libraries=drawing";
             script.async = true;
             script.onload = initMap;
             document.head.appendChild(script);
